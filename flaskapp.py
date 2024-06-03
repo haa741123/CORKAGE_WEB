6818be67b90c4a719067774e2976891feb5369ee
haa741123 <66108236+haa741123@users.noreply.github.com>
2024-06-04 06:22:08 +0900

    login.html 렌더링

--- flaskapp.py
+++ flaskapp.py
@@ -1,11 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template('/assets/html/index.html')
-    
+
+@app.route("/login")
+def login():
+    return render_template('/assets/html/login.html')
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
