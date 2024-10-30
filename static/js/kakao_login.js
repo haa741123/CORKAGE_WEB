f1e9533ff48e766219530a7da96009c96964b0f8
wjswlgns03 <jjh030726@tukorea.ac.kr>
2024-10-30 16:03:21 +0900

    "카카오 로그인 수정"

--- static/js/kakao_login.js
+++ static/js/kakao_login.js
@@ -1,3 +1,37 @@
+const slider = document.getElementById('slider');
+const dots = document.querySelectorAll('.dot');
+let isScrolling = false;
+
+// 슬라이더가 스크롤 될 때마다 인디케이터 업데이트
+slider.addEventListener('scroll', function() {
+    if (!isScrolling) {
+        window.requestAnimationFrame(() => {
+            const scrollPosition = slider.scrollLeft;
+            const sliderWidth = slider.clientWidth;
+            const currentIndex = Math.round(scrollPosition / sliderWidth);
+
+            dots.forEach((dot, index) => {
+                if (index === currentIndex) {
+                    dot.classList.add('active');
+                } else {
+                    dot.classList.remove('active');
+                }
+            });
+
+            isScrolling = false;
+        });
+    }
+    isScrolling = true;
+});
+
+// 첫 번째 인디케이터 활성화
+dots[0].classList.add('active');
+
+
+
+
+
+
 const REST_API_KEY = '6b5cc3ff382b0cb3ea15795729b3329f';
 
 // 테스트 환경
@@ -10,5 +44,3 @@ document.getElementById('kakao-login-btn').addEventListener('click', function() 
     const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
     window.location.href = KAKAO_AUTH_URL;
 });
-
-
